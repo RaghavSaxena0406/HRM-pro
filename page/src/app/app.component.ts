0297f0e2ad77d6a08cd7f3cf2b32bb17a51fc96a
mohit <mohitsr4200@gmail.com>
2024-09-22 16:20:45 +0530

    adding login/register/option

--- page/src/app/app.component.ts
+++ page/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { OptionComponent } from './option/option.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FirstComponent,OptionComponent,LoginComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'page';
+  constructor(private router:Router){}
 }
+
--- page/src/app/app.routes.ts
+++ page/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { NgModule } from '@angular/core';
+import { OptionComponent } from './option/option.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'option', component: OptionComponent},
+    { path:'login',component:LoginComponent},
+    { path:'register', component:RegisterComponent}
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+export class AppRoutingModule { }
